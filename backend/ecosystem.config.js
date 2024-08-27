3454350d8386e5bcfcd25a0534a5b65fd7fce2a3
Zyurkalov <Zyurkalov@gmail.com>
2024-08-28 00:07:53 +0500

    fix setting for deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,13 +18,8 @@ deploy: {
     repo: DEPLOY_REPO,
     path: DEPLOY_PATH,
     // 'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-    'pre-deploy': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-    'post-deploy':
-      `cd backend &&
-      pwd &&
-      npm ci
-      && npm run build
-      && pm2 startOrRestart ecosystem.config.js --env production`,
+    'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+    'post-deploy': "cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
   },
 },
 }
